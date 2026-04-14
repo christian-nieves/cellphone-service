74d886e401458e7b7aed63a56129a21a3d4afff7
christian-nieves <thechristiannieves@gmail.com>
2026-04-14 15:35:50 -0400

    Final Commit

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -9,19 +9,36 @@ public class CellPhoneApplication {
 
         System.out.print("What is the serial number?: ");
         int serialNum = scanner.nextInt();
+        phone.setSerialNumber(serialNum);
         scanner.nextLine();
 
         System.out.print("What model is the phone?: ");
         String phoneModel = scanner.nextLine();
+        phone.setModel(phoneModel);
 
         System.out.print("Who is the carrier?: ");
         String carrier = scanner.nextLine();
+        phone.setCarrier(carrier);
 
         System.out.print("What is the phone number?: ");
         String phoneNum = scanner.nextLine();
+        phone.setPhoneNumber(phoneNum);
 
-        System.out.println("Who is the owner of the phone?: ");
+        System.out.print("Who is the owner of the phone?: ");
         String owner = scanner.nextLine();
+        phone.setOwner(owner);
+
+
+        System.out.println("\nHere is your phone information: ");
+
+        System.out.println(phone.getSerialNumber());
+        System.out.println(phone.getModel());
+        System.out.println(phone.getCarrier());
+        System.out.println(phone.getPhoneNumber());
+        System.out.println(phone.getOwner());
+
+        scanner.close();
+
 
 
 

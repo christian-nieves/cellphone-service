e31295903322a8a229dd21188dfe7b32170801c0
christian-nieves <thechristiannieves@gmail.com>
2026-04-14 15:08:08 -0400

    CellPhone.Java done

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CellPhone phone = new CellPhone();
+
+        System.out.print("What is the serial number?: ");
+
+
+
+
+    }
 }

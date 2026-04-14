d989f1938e028fd573b5d2eed9333e2e15604440
christian-nieves <thechristiannieves@gmail.com>
2026-04-14 15:21:04 -0400

    Asking user questions done

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -8,6 +8,23 @@ public class CellPhoneApplication {
         CellPhone phone = new CellPhone();
 
         System.out.print("What is the serial number?: ");
+        int serialNum = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("What model is the phone?: ");
+        String phoneModel = scanner.nextLine();
+
+        System.out.print("Who is the carrier?: ");
+        String carrier = scanner.nextLine();
+
+        System.out.print("What is the phone number?: ");
+        String phoneNum = scanner.nextLine();
+
+        System.out.println("Who is the owner of the phone?: ");
+        String owner = scanner.nextLine();
+
+
+
 
 
 
